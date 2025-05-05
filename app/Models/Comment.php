661db1fd7a2607341c129465855e91b212fa8be7
FrancoLelli <francogastonlelli@gmail.com>
2025-05-05 12:20:56 -0300

    Estructura del proyecto

---
+++ app/Models/Comment.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $connection = 'mongodb';
+
+    protected $table = 'comments';
+
+    protected $fillable = [
+        'comment',
+        'user_id',
+        'post_id',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+}
